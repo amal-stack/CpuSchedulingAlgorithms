d8ce84bf7c3497b7367526b5082221f0d2ccb80a
amalkrishna <amalkrishna263@outlook.com>
2022-02-19 18:18:35 +0530

    Add non-preemptive priority scheduler

---
+++ CpuSchedulingAlgorithms/PriorityScheduler.cs
@@ -0,0 +1,73 @@
+﻿namespace CpuSchedulingAlgorithms;
+
+/// <summary>
+/// Non-preemptive priority scheduler.
+/// </summary>
+public class PriorityScheduler : IProcessScheduler
+{
+    private readonly ArrivalSchedule schedule;
+
+    private readonly PriorityQueue<ProcessControlBlock, int> readyQueue = new();
+
+    private readonly List<CompletedProcess> completedProcesses = new();
+
+    private bool HasNextProcess => readyQueue.Count > 0;
+
+    public IReadOnlyList<CompletedProcess> CompletedProcesses => completedProcesses.AsReadOnly();
+
+    public ProcessTimeline Timeline { get; } = new();
+
+    public ProcessControlBlock? CurrentProcess { get; private set; }
+
+    public int Now { get; private set; }
+
+    public ProcessQueueView RunQueue => new(readyQueue.UnorderedItems.Select(i => i.Element));
+
+    public PriorityScheduler(ArrivalSchedule schedule)
+    {
+        this.schedule = schedule;
+    }
+
+    /// <inheritdoc/>
+    public bool Proceed()
+    {
+        // Return false because no more processes will arrive if:
+        // * no current process exists or if the current process is complete and
+        // * no next process (ready queue is empty) and
+        // * current time quantum is more than the schedule end time/maximum arrival time of all processes) 
+        if (CurrentProcess is null or { IsComplete: true } && !HasNextProcess && Now > schedule.EndTime)
+        {
+            return false;
+        }
+
+        // Add processes arrived at current time(Now) to the ready queue
+        if (Now <= schedule.EndTime)
+        {
+            foreach (var process in schedule.GetProcessesArrivedAt(Now))
+            {
+                readyQueue.Enqueue(new ProcessControlBlock(process, arrivalTime: Now), process.Priority);
+            }
+        }
+
+        // If no current process exists or if the current process is complete, get next process if exists
+        if (CurrentProcess is null or { IsComplete: true })
+        {
+            CurrentProcess = HasNextProcess ? readyQueue.Dequeue() : null;
+        }
+
+        // Execute the current process and increment to the next time quantum
+        CurrentProcess?.Execute(Now);
+        Now++;
+
+        // Add the process that consumed the current time quantum to the timeline 
+        Timeline.Add(Now, CurrentProcess);
+
+        // If process is complete, add process to the list of completed processes
+        if (CurrentProcess is { IsComplete: true })
+        {
+            completedProcesses.Add(CompletedProcess.FromProcessControlBlock(CurrentProcess, Now));
+        }
+
+        return true;
+    }
+}
